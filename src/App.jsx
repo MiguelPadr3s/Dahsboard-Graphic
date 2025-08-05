1ba695d5fbec65ac42af6a02281a505faeb00698
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-08-05 16:36:45 -0700

    form

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Dashboard from "./scenes/dashboard/index.jsx";
 import MySidebar from "./scenes/global/MySidebar";
 import Team from "./scenes/team/index.jsx";
 import Contacts from "./scenes/contacts/index.jsx";
+import Invoices from "./scenes/invoices/index.jsx";
+import Form from "./scenes/form/index.jsx";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -22,6 +25,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts/>}/>
+              <Route path="/invoices" element={<Invoices/>}/>
+              <Route path="/form" element={<Form/>}/>
             </Routes>
           </main>
         </div>
