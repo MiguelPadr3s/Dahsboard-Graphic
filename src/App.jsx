9e2993f282082d1dffb52d8ec4192e1f5d674749
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-08-05 10:42:47 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Dashboard from "./scenes/dashboard/index";
+import MySidebar from "./scenes/global/MySidebar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <MySidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
