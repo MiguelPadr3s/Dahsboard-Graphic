5b81f7af83db0222816e760394af589a5d25dca4
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-08-05 15:55:37 -0700

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Dashboard from "./scenes/dashboard/index.jsx";
 import MySidebar from "./scenes/global/MySidebar";
 import Team from "./scenes/team/index.jsx";
+import Contacts from "./scenes/contacts/index.jsx";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -20,6 +21,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts/>}/>
             </Routes>
           </main>
         </div>
